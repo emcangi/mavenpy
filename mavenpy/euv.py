5993e00ca9beb109361571ef7efe634385925f58
Rebecca Jolitz <rjolitz@berkeley.edu>
2024-07-17 17:26:39 -0700

    (euv) now works for level 2 euv data, includes units for l2 data, accounts for different names in CDF structure, option to remove errant EUV data which is marked with -1e31

--- mavenpy/euv.py
+++ mavenpy/euv.py
@@ -1,3 +1,7 @@
+import os
+
+import numpy as np
+
 from .read import read_cdf
 from .helper import process_data_dict
 
@@ -6,58 +10,112 @@ from .helper import process_data_dict
 
 # Version / revision information for EUV:
 # - v## refers to the file version, which changes if CDF fields are modified.
-#    As of 8/23/23, it is locked at v04.
+#    As of 6/17/23, it is locked at v15.
 # - r## refers to the version of data available for that version.
 # want the newest version & revision.
 
-units = {"epoch": "utc", "x": "unx", "flag": "usability flag",
-         "maven_sun_distance": "km", "v": "nm",
-         "y": "W/m2/nm", "uncertainty": "%",
-         "irradiance": "W/m2/nm"}
+# Units for EUV based on the SIS:
+units = {"epoch": "utc", "time": "unx",
+         "flag": "usability flag",
+         "maven_sun_distance": "km",
+         "spectral_irradiance": "W/m2/nm",
+         "wavelength": "nm",
+         "uncertainty": "%",
+         "irradiance": "W/m2 (17-22 nm, 0-7 nm, 121-122 nm)",
+         "precision": "%",
+         "accuracy": "%",
+         "counts": "#/s"}
 
-column_to_field_name = {"time": "x", "wavelength": "v", "spectra": "y"}
-field_name_to_column = {v: k for k, v in column_to_field_name.items()}
-modified_labels = column_to_field_name.keys()
+# Mapping from common column names to the fields in a given
+# CDF:
+column_to_field_name =\
+    {"l2": {"irradiance": "data", "precision": "ddata", "accuracy": "dfreq",
+            "time": "time_unix"},
+     "l3": {"time": "x", "wavelength": "v", "spectral_irradiance": "y"}}
+
+# Inversion of the field name, update the units for field name mapping
+field_to_column_name = {}
+units_by_field_name = {}
+for level in column_to_field_name:
+    column_to_field_i = column_to_field_name[level]
+    field_to_column_name[level] = {v: k for k, v in column_to_field_i.items()}
+    units.update({v: units[k] for k, v in column_to_field_i.items()})
 
 
-def read(dataset_filename, lib='cdflib',
-         column_names=("time", "epoch", "wavelength", "spectra", "flag"),
-         include_unit=True, relabel_columns=True):
+def read(dataset_filename, lib='cdflib', level='', field_names="",
+         column_names="", include_unit=True, relabel_columns=True,
+         NaN_errant_data=True):
 
-    """Loads daily files of EUV Level 3 data.
+    """Loads daily files of EUV Level 3 and 2 data.
 
     lib: string, 'cdflib' or 'spacepy'
+    field_names: CDF variable names, e.g. 'v'
+    column_names: variable names, e.g. 'irradiance'
+    relabel_columns: Boolean to enable relabeling the fields into
+        more common column names:
+    include_unit: returns a tuple for each dataset with the unit
 
     This routine is based on the EUV SIS document and the MAVEN SPEDAS
     routine 'mvn_euv_l3_load.pro'. We thank and credit the authors of both.
+
+    L2 data consists of solar irradiances in three bandpasses calibrated
+    for background signal, out-of-band signal, normalized gain, etc.
+    Uncertainties for each irradiance also provided, with a flag indicating if
+    EUV has favorable pointing. Also includes corrected count rates pre-
+    conversion to irradiance units.
 
     L3 data from EUV consists of outputs of the FISM-M model based on
     EUV calibrated band irradiance. This includes an averaged spectra
     (day or minute) as a function of time, a quality flag,
     and the Mars-sun distance at time of observation.
 
-    wavelength: wavelengths that the spectra is determined on in
-        units of nanometer, (190)
-    spectra: averaged spectra in units of Watts per square meter per nanometer,
-             2D dataset of dimensions (190 x n_time)
-    epoch: datetime of observation, (n_time)
-    unx_time: Number of seconds elapsed since 1970-01-01/00:00:00, (n_time)
     flag: integer representing data useability
          (0 - good, 1 - occultation, 2 - no pointing, 3 - sun partial in FOV,
           4 - sun not in FOV, 5 - windowed,  6 - eclipse, 7 - spare), (n_time)
-    maven_sun_distance: distance between MAVEN and sun in kilometers, (n_time)
 
     """
 
-    field_names = [column_to_field_name[i] if i in modified_labels
-                   else i for i in column_names]
+    # If not provided the level, recover that info from the filename:
+    if not level:
+        filename = os.path.split(dataset_filename)[1]
+        level = filename.split("_")[2]
+
+    # If not provided the fields to retrieve, generate from the column names
+    # requested. Else, use the defaults:
+    if not field_names:
+        if column_names:
+            column_to_field_name_i = column_to_field_name[level]
+            field_names = [column_to_field_name_i[i] if i in
+                           column_to_field_name_i else i for i in column_names]
+        else:
+            field_to_column_name_i = field_to_column_name[level]
+            if level == 'l3':
+                field_names = ("x", "epoch", "v", "y", "flag")
+            elif level == 'l2':
+                field_names = ('data', 'ddata', 'dfreq', 'flag',
+                               'time_unix', 'epoch', 'maven_sun_distance',
+                               'counts')
+            column_names = [field_to_column_name_i[i] if i in
+                            field_to_column_name_i else i for i in field_names]
+
+    # Read CDF:
     data = read_cdf(dataset_filename, field_names, lib=lib)
+
+    # Errant EUV data filled with -1 E 31
+    if NaN_errant_data:
+        for i in data:
+            if "epoch" in i or "flag" in i:
+                continue
+            # print(i)
+            data_i = data[i]
+            errant = (data_i < -1e30)
+            data_i[errant] = np.nan
 
     # Data relabeled according to new column names
     # Include unit if requested:
 
     data = process_data_dict(
         data, units=(units if include_unit else None),
-        alias=(field_name_to_column if relabel_columns else None))
+        alias=(field_to_column_name_i if relabel_columns else None))
 
     return data
